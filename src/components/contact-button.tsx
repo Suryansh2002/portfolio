fba54498b98e3292ca303937026f88d57c500617
Suryansh2002 <suryanshwins2002@gmail.com>
2024-05-19 11:09:07 +0530

    fix gradient

--- src/components/contact-button.tsx
+++ src/components/contact-button.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 function Socials({cls, setcls}:{cls:string, setcls:Function}){
     return <div className={`
-        ${cls} absolute left-1/2 -translate-x-1/2 top-1/2 h-52 w-96 md:w-1/4 bg-indigo-900 rounded-xl border-2 border-blue-500 shadow-[0px_0px_14px_blue] 
+        ${cls} absolute left-1/2 -translate-x-1/2 top-1/2 h-52 w-96 md:w-1/4 bg-gradient-to-br from-violet-800 to-blue-950 rounded-xl border-2 border-blue-500 shadow-[0px_0px_14px_blue] 
         text-white flex justify-center items-center overflow-hidden
     `}>
-        <button className="absolute top-0 right-0 h-5 w-7 rounded-md pb-6 bg-indigo-950 hover:bg-red-600" 
+        <button className="absolute top-0 right-0 h-7 w-7 rounded-md  bg-indigo-950 hover:text-red-700 text-neutral-200 text-sm"  
             onClick={()=>{setcls("hidden")}}
         >
-            x
+            ✕
         </button>
         <a href="https://github.com/Suryansh2002">
             <Image src="/github.svg" alt="Github" width={100} height={100}/>
