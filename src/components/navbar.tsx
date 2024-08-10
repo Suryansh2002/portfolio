87be8f4b8303dcbf83ffc99260c4292ef5708f0a
Suryansh2002 <suryanshwins2002@gmail.com>
2024-08-10 18:37:32 +0530

    fixing navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar(){
     return <nav className="h-16 w-screen bg-slate-900 border-b-2 border-blue-500 rounded-md overflow-hidden z-10 shadow-[0px_0px_6px_rgba(150,150,250)]">
        {links.map((link)=>{
             return <Link key={link.name} href={link.url} className={`
-                inline-block ml-7 text-xl my-2 px-3 py-2 rounded-lg
+                inline-block ml-3 text-xl my-2 px-3 py-2 rounded-lg
                 text-white hover:bg-black-950 hover:shadow-[0px_0px_6px_rgba(150,150,250)] hover:border-2 hover:border-blue-500 hover:bg-neutral-900
                 transition-all duration-100
             `}>
