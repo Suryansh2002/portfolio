20946c7c0175c1bacdbbb20ac5ecba6436013b53
Suryansh2002 <suryanshwins2002@gmail.com>
2024-11-11 00:41:01 +0530

    fix textarea

--- src/components/ui/send-message.tsx
+++ src/components/ui/send-message.tsx
@@ -13,8 +13,8 @@ export function SendMessage({setPollIn, email}:{setPollIn: Dispatch<SetStateActi
         };
         setPollIn(300);
     }
-    return <form className="w-full flex justify-center gap-2 absolute bottom-4 left-0 right-0 px-4" onSubmit={submit}>
-        <textarea className="flex flex-1 ml-2 bg-slate-900 text-white text-xl rounded-3xl pt-1 border-blue-700 border-2 shadow-[0px_0px_6px_rgba(150,150,250)] px-5 py-2 no-scrollbar" name="text" id="text"></textarea>
+    return <form className="flex justify-center gap-2 absolute bottom-4 left-0 right-0 px-4" onSubmit={submit}>
+        <textarea className="flex w-[800px] flex-shrink ml-2 bg-slate-900 text-white text-xl rounded-3xl pt-4 border-blue-700 border-2 shadow-[0px_0px_6px_rgba(150,150,250)] px-5 no-scrollbar" name="text" id="text"></textarea>
         <button type="submit" className="bg-indigo-400 mr-2 rounded-3xl w-16 flex-shrink-0 flex justify-center items-center border-blue-800 border-4">
             <Image src={"/send.svg"} alt="send" width={40} height={30}/>
         </button>
--- src/components/ui/show-messages.tsx
+++ src/components/ui/show-messages.tsx
@@ -12,8 +12,8 @@ export function ShowMessages({messages, isAdmin}:{messages: Message[], isAdmin?:
 
     return <div className="overflow-x-hidden overflow-y-scroll no-scrollbar h-full text-white" ref={messagesRef}>
         {messages.map((msg, i)=>{
-            return <div key={i} className={`flex ${msg.from == from?"justify-end ":"justify-start"} p-2`}>
-                <div className={`p-4 rounded-lg ${msg.from == from? "bg-emerald-500": "bg-blue-400"} md:max-w-[40%] max-w-[80%] break-words`}>
+            return <div key={i} className={`flex ${msg.from == from?"justify-end ":"justify-start"} p-2 px-6`}>
+                <div className={`p-4 rounded-lg ${msg.from == from? "bg-emerald-600": "bg-blue-400"} md:max-w-[40%] max-w-[80%] break-words`}>
                     {msg.message}
                 </div>
             </div>
